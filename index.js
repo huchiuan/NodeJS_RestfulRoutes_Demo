ccefb5ee8e6f3b2b778027320f038fe6ea846c6e
huchiuan <tusna08124@gmail.com>
2021-02-17 00:37:12 +0800

    <使用UUID套件製作ID>

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const app = express ();
 const path = require('path');
+const { v4:uuid } = require('uuid');
+
+
 
 app.use (express.urlencoded({ extended : true}))
 //上行是用來處理request 資料用的 叫express解析
@@ -11,27 +14,27 @@ app.set('view engine','ejs')
 
 const comments=[
     {
-        id:1,
+        id:uuid(),
         username: 'Tod',
         comment:'不好笑'
     },
     {   
-        id:2,
+        id:uuid(),
         username: 'AAd',
         comment:'好笑'
     },
     {
-        id:3,
+        id:uuid(),
         username: '哥哥',
         comment:'普通'
     },
     {
-        id:4,
+        id:uuid(),
         username: '子瑜',
         comment:'讚'
     },
     {
-        id:5,
+        id:uuid(),
         username: '狗',
         comment:'不太有趣'
     },
@@ -45,14 +48,14 @@ app.get('/comments/new',(req,res)=>{
 })
 app.post('/comments',(req,res)=>{
     const { username,comment} =req.body;
-    comments.push({username,comment});
+    comments.push({username,comment,id: uuid()});
    
     res.redirect('/comments');
 })
 
 app.get('/comments/:id',(req,res)=>{
    const {id}= req.params;
-   const comment = comments.find(c=>c.id === parseInt(id));
+   const comment = comments.find(c=>c.id === id);
 
    res.render('comments/show',{comment})
 })
